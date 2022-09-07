130bb976a4a043a249afa847d057c13c5a25558f
Yaswanth <pyaswanth4@gmail.com>
2022-09-07 09:15:31 +0530

    q

--- DBExamples/Form3.cs
+++ DBExamples/Form3.cs
@@ -22,34 +22,7 @@ namespace DBExamples
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            con = new SqlConnection("Data Source=DESKTOP-1G6BPHF;Database=CSDB;Integrated Security=SSPI");
-            cmd = new SqlCommand();
-            cmd.Connection = con;
-            con.Open();
-            LoadData();
+         
         }
-        private void LoadData()
-        {
-            cmd.CommandText = "Select Eno, Ename, Job, Salary, Status From Employee Where Status=1 Order By Eno";
-            dr = cmd.ExecuteReader();
-            ShowData();
-        }
-        private void ShowData()
-        {
-            if (dr.Read())
-            {
-                textBox1.Text = dr["Eno"].ToString();
-                textBox2.Text = dr["Ename"].ToString();
-                textBox3.Text = dr["Job"].ToString();
-                textBox4.Text = dr["Salary"].ToString();
-                checkBox1.Checked = (bool)dr["Status"];
-            }
-            else
-            {
-                MessageBox.Show("You are at the last record of table.", "Information", MessageBoxButtons.OK,
-            MessageBoxIcon.Information);
-            }
-        }
-
     }
 }
