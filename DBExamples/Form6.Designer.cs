f8bd5fa8f9f103789a6146765c1ec53557bcce91
Yaswanth <pyaswanth4@gmail.com>
2022-09-13 09:44:34 +0530

    .

---
+++ DBExamples/Form6.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DBExamples
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSaveDb = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(677, 376);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnClose.Location = new System.Drawing.Point(491, 387);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(136, 51);
+            this.btnClose.TabIndex = 40;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveDb
+            // 
+            this.btnSaveDb.BackColor = System.Drawing.SystemColors.Info;
+            this.btnSaveDb.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnSaveDb.Location = new System.Drawing.Point(43, 387);
+            this.btnSaveDb.Name = "btnSaveDb";
+            this.btnSaveDb.Size = new System.Drawing.Size(303, 51);
+            this.btnSaveDb.TabIndex = 39;
+            this.btnSaveDb.Text = "Save To DataBase";
+            this.btnSaveDb.UseVisualStyleBackColor = false;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSaveDb);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form6";
+            this.Text = "Form6";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSaveDb;
+    }
+}
---
+++ DBExamples/Form6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;  
+namespace DBExamples
+{
+    public partial class Form6 : Form
+    {
+        DataSet ds;
+        SqlDataAdapter da;
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
