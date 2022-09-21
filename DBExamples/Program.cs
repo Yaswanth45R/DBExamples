b24187f48736f8ab18bed79b7d28fee04f697c88
Yaswanth <pyaswanth4@gmail.com>
2022-09-21 10:07:28 +0530

    .

--- DBExamples/Program.cs
+++ DBExamples/Program.cs
@@ -16,7 +16,7 @@ namespace DBExamples
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form11());
+            Application.Run(new Form12());
         }
     }
 }
